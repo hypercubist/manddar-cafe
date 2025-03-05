e6fc27c12e897c028bc11c2a6e742c2f6df99ac2
manddar <qpinguz@gmail.com>
2025-03-06 00:11:25 +0900

    스프링부트 백엔드 프로젝트 생성

---
+++ cafe-order-backend/src/main/java/com/manddar/cafe_order_backend/CafeOrderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.manddar.cafe_order_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeOrderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeOrderBackendApplication.class, args);
+	}
+
+}
---
+++ cafe-order-backend/src/test/java/com/manddar/cafe_order_backend/CafeOrderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manddar.cafe_order_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeOrderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
